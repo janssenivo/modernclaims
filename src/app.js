5731be1ef305eac25751e6106ae3afaf32f3a767
Ivo Janssen <ivo.janssen@gmail.com>
2015-04-20 19:17:56 -0400

    initial checkin

---
+++ src/app.js
@@ -0,0 +1,45 @@
+/**
+ * Test app to demonstrate Kinvey integration for CityWatch app on Pebble
+ *
+ * Ivo Janssen, 2015-04-12
+ */
+
+var UI = require('ui');
+//var Vector2 = require('vector2');
+var ajax = require('ajax');
+
+var main = new UI.Card({
+  title: ' MODERN',  icon: 'images/kinvey.png',
+  subtitle: 'Claims App',  body: 'Press middle button to continue.'
+});
+
+main.show();
+
+main.on('click', 'select', function(e) {
+  var menu = new UI.Menu({
+    sections: [{
+      title: 'Claims'
+    }]
+  });
+  
+  var claimitems = [];
+  ajax({ url: 'https://baas.kinvey.com/appdata/kid_WkiTdocbR/CityWatch', type: 'json', 
+         headers: {'Authorization':'Basic a2lkX1draVRkb2NiUjpkNGZjNGNkYjQ0YWQ0OGNmYWM2NGVmNTAzNDUwNDk2Mw=='} },
+         function(data) {
+      for (var i = 0, len = data.length; i < len; i++) {
+        claimitems.push({title:data[i].category, subtitle:data[i].title, 
+                      description:data[i].description,address:data[i].description});
+      }
+  });
+  menu.items(0,claimitems);
+         
+  menu.on('select', function(p) {
+    var card = new UI.Card();
+    card.body(p.item.description+" address:"+p.item.address);
+    card.title(p.item.title);
+    card.show();  
+  });
+  menu.show();
+  
+});
+
